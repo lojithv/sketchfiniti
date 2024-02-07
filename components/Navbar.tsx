a85405b79fbaf194dcfbc4041ef7f1b492fe1f47
lojith <lojithvinsuka1998@gmail.com>
2024-02-07 18:11:53 +0530

    Update product name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     const user = null
     return (
         <div className="bg-slate-900 w-full text-slate-50 p-5 flex gap-5 justify-between">
-            <div>Plannr</div>
+            <div>Sketchfiniti</div>
             {!user && <div className="flex gap-5">
                 <div>Sign In</div>
             </div>}
