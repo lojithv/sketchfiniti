f5d2f2fca5ec300643885f9dce4c437b4135a568
lojith <lojithvinsuka1998@gmail.com>
2024-02-07 17:59:09 +0530

    Refactor Navbar component to conditionally render Sign In and Sign Up buttons based on user status

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
 export default function Navbar() {
-
+    const user = null
     return (
         <div className="bg-slate-900 w-full text-slate-50 p-5 flex gap-5 justify-between">
             <div>Plannr</div>
-            <div className="flex gap-5">
+            {!user && <div className="flex gap-5">
                 <div>Sign In</div>
-                <div>Sign Up</div>
-            </div>
-            <div>Dashboard</div>
+            </div>}
+            {user &&
+                <div>Dashboard</div>
+            }
         </div>
     )
 }
