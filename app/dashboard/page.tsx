445b956b8a3d28ef1c27f1a7c12aa427e8b713ed
lojith <lojithvinsuka1998@gmail.com>
2024-02-10 14:41:48 +0530

    Add AuthProvider and update Navbar component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "@/components/Navbar";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function Dashboard() {
     return (
-        <div className="flex justify-center flex-col items-center">
-            <Navbar />
-            Dashboard
-        </div>
+        <AuthProvider>
+            <div className="flex justify-center flex-col items-center">
+                <Navbar />
+                Dashboard
+            </div>
+        </AuthProvider>
     )
 }
