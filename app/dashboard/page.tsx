0fda5ef176770366096b7273f27d765397ccd4cf
lojith <lojithvinsuka1998@gmail.com>
2024-02-10 14:52:18 +0530

    Update page layouts and add ProjectsList component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,16 @@
+"use client"
 import Navbar from "@/components/Navbar";
+import ProjetcsList from "@/components/ProjetcsList";
 import { AuthProvider } from "@/context/AuthContext";
 
 export default function Dashboard() {
+
+
     return (
         <AuthProvider>
-            <div className="flex justify-center flex-col items-center">
+            <div className="flex w-screen h-screen justify-center flex-col items-center">
                 <Navbar />
-                Dashboard
+                <ProjetcsList />
             </div>
         </AuthProvider>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { AuthProvider } from "@/context/AuthContext";
 export default function Home() {
   return (
     <AuthProvider>
-      <div className="flex justify-center flex-col items-center">
+      <div className="flex w-screen h-screen justify-center flex-col items-center">
         <Navbar />
         Home
       </div>
