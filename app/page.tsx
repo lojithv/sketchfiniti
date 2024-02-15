16ce15fbe2063755e8ced6f23e795cb450398ed6
lojith <lojithvinsuka1998@gmail.com>
2024-02-15 07:59:01 +0530

    Refactor page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { AuthProvider } from "@/context/AuthContext";
 export default function Home() {
   return (
     <AuthProvider>
-      <div className="flex w-screen h-screen justify-center flex-col items-center">
+      <div className="flex w-screen h-screen flex-col">
         <Navbar />
         Home
       </div>
