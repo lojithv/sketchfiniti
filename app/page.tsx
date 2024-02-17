1213779aea9189e2df4509dae34f43cab4ba0316
lojith <lojithvinsuka1998@gmail.com>
2024-02-17 10:36:56 +0530

    Add Adobe React Spectrum theme and update Navbar styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import Navbar from "@/components/Navbar";
 import { AuthProvider } from "@/context/AuthContext";
+import { Provider, defaultTheme } from "@adobe/react-spectrum";
 
 export default function Home() {
   return (
-    <AuthProvider>
-      <div className="flex w-screen h-screen flex-col">
-        <Navbar />
-        Home
-      </div>
-    </AuthProvider>
+    <Provider theme={defaultTheme}>
+      <AuthProvider>
+        <div className="flex w-screen h-screen flex-col bg-slate-900">
+          <Navbar />
+          Home
+        </div>
+      </AuthProvider>
+    </Provider>
   )
 }
